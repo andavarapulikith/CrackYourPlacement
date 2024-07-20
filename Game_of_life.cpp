20e7507bc508de8980ea7859c3ad253e0a9376ec
andavarapulikith <likithandavarapuiiits@gmail.com>
2024-07-20 17:29:08 +0530

    added problem 24

---
+++ Game_of_life.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               int cnt=0;
+                for(int k=i-1;k<=i+1;k++){
+                    for(int l=j-1;l<=j+1;l++){
+                        if(k==i && l==j)
+                        continue;
+                        if(k>=0 && k<n && l>=0 && l<m && (board[k][l]==1 || board[k][l]==3))
+                        cnt++;
+
+                    }
+                }
+                if(board[i][j]==1 && (cnt <2 || cnt>3) )
+                board[i][j]=3;
+                if(board[i][j]==0 && cnt==3)
+                board[i][j]=2;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j]==3)
+                board[i][j]=0;
+                else if(board[i][j]==2)
+                board[i][j]=1;
+            }
+        }
+        
+    }
+};
