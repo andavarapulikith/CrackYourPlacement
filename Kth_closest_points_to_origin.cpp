ecd117f926071edd858c8ed971ee1fee6974ab1a
andavarapulikith <likithandavarapuiiits@gmail.com>
2024-08-22 09:53:21 +0530

    added problem 200

---
+++ Kth_closest_points_to_origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,pair<int,int>>>pq;
+        int n=points.size();
+        for(int i=0;i<n;i++){
+            int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            pq.push({dist,{points[i][0],points[i][1]}});
+            if(pq.size()>k)
+            pq.pop();
+
+        }
+        vector<vector<int>>ans;
+        while(!pq.empty()){
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        return ans;
+        
+    }
+};
